6e3a731325adcccc26953d787f5bb569bf7f91dc
DasminX <wojtek.test99@gmail.com>
2026-05-13 21:04:06 +0200

    chore: catching car mileage numbers kata (pt. 2 done)

--- catching-car-mileage-numbers/script.ts
+++ catching-car-mileage-numbers/script.ts
@@ -3,76 +3,20 @@ export function isInteresting(n: number, awesomePhrases: number[]): number {
   if (awesomePhrases.indexOf(n) >= 0) return 2;
   if (awesomePhrases.some((num) => num - n <= 2 && num > n)) return 1;
 
-  const ascendingPatternResult = matchesAscendingPattern(n);
-  if (ascendingPatternResult) return ascendingPatternResult;
-
-  const descendingPatternResult = matchesDescendingPattern(n);
-  if (descendingPatternResult) return descendingPatternResult;
-
   if (matchesSimplePatterns(n)) return 2;
   if ([1, 2].some((val) => matchesSimplePatterns(n + val))) return 1;
 
   return 0;
 }
 
+// NOTE: changed the ascending/descending pattern implementation after solving the kata, based on other solutions.
+// Original solution in previous commit.
 function matchesSimplePatterns(n: number) {
   if (n < 100) return false;
   const nass = n.toString();
   if (nass === nass.split("").reverse().join("")) return true; // Palindrome matched
   if (nass.match(/^[0-9]{1}0+$/gi)) return true; // Any digit followed by all zeros
-  return false;
-}
-
-function matchesAscendingPattern(n: number) {
-  if (n < 100) return 0;
-
-  let output = 0;
-
-  for (let i = 0; i <= 2; i++) {
-    let innerOutput = 2;
-    const split = (n + i).toString().split("");
-    for (let j = 1; j < split.length; j++) {
-      const calculation = +split[j] - +split[j - 1];
-      if (calculation === 1 || (calculation === -9 && j === split.length - 1)) continue;
-      innerOutput = 0;
-      break;
-    }
-    if (innerOutput === 2) {
-      if (i === 0) {
-        output = 2;
-        break;
-      }
-      output = 1;
-    }
-  }
-
-  return output;
-}
-
-function matchesDescendingPattern(n: number) {
-  if (n < 100) return 0;
-
-  let output = 0;
-
-  for (let i = 0; i <= 2; i++) {
-    let innerOutput = 2;
-    const split = (n + i).toString().split("");
-    for (let j = 1; j < split.length; j++) {
-      const calculation = +split[j] - +split[j - 1];
-      if (calculation === -1 || (calculation === 9 && j === split.length - 2)) continue;
-      innerOutput = 0;
-      break;
-    }
-    if (innerOutput === 2) {
-      if (i === 0) {
-        output = 2;
-        break;
-      }
-      output = 1;
-    }
-  }
-
-  return output;
+  return "1234567890".includes(nass) || "9876543210".includes(nass) || false;
 }
 
 // near ascending sequence
@@ -90,18 +34,18 @@ console.log(isInteresting(7890, []) === 2); // 2 DONE
 console.log(isInteresting(7891, []) === 0); // 0 DONE
 
 // near descending sequence
-console.log(isInteresting(5429, []) === 0); // 0
-console.log(isInteresting(5430, []) === 1); // 1
-console.log(isInteresting(5431, []) === 1); // 1
-console.log(isInteresting(5432, []) === 2); // 2
-console.log(isInteresting(5433, []) === 0); // 0
+console.log(isInteresting(5429, []) === 0); // 0 DONE
+console.log(isInteresting(5430, []) === 1); // 1 DONE
+console.log(isInteresting(5431, []) === 1); // 1 DONE
+console.log(isInteresting(5432, []) === 2); // 2 DONE
+console.log(isInteresting(5433, []) === 0); // 0 DONE
 
 // near descending sequence (edge case with 0-9)
-console.log(isInteresting(3207, []) === 0); // 0
-console.log(isInteresting(3208, []) === 1); // 1
-console.log(isInteresting(3209, []) === 1); // 1
-console.log(isInteresting(3210, []) === 2); // 2
-console.log(isInteresting(3211, []) === 0); // 0
+console.log(isInteresting(3207, []) === 0); // 0 DONE
+console.log(isInteresting(3208, []) === 1); // 1 DONE
+console.log(isInteresting(3209, []) === 1); // 1 DONE
+console.log(isInteresting(3210, []) === 2); // 2 DONE
+console.log(isInteresting(3211, []) === 0); // 0 DONE
 
 // "boring" numbers
 console.log(isInteresting(3, [1337, 256]) === 0); // 0 DONE
